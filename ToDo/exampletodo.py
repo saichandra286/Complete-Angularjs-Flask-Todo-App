413c040283cbbddf0309b74d04c1c5b2c5991066
saichandra <saichandra286@gmail.com>
2015-12-31 14:38:01 +0530

    Inital todo list

---
+++ ToDo/exampletodo.py
@@ -0,0 +1,33 @@
+tasks = [
+    {
+        'id': 1,
+        'title': 'Learn Angularjs',
+        'description': 'Milk, Cheese, Pizza, Fruit, Tylenol'
+    },
+    {
+        'id': 2,
+        'title': 'Learn Python with flask',
+        'description': 'complete flask'
+    },
+    {
+        'id': 3,
+        'title': 'Build TODO app',
+        'description': 'front end wid Angularjs and backend flask'
+
+    },
+    {
+        'id': 4,
+        'title': 'Contact satya Bro',
+        'description': 'need to dissucc features of my app'
+    },
+    {
+        'id': 5,
+        'title': 'Get Interview',
+        'description': 'get Interview date'
+    },
+    {
+        'id': 6,
+        'title': 'Got placed in dream',
+        'description': 'get offer letter'
+    }
+]
